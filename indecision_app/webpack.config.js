2faf4a85c9f81e0c64450703123c9c756c1bf675
Dan Spector <danno2423@gmail.com>
2018-12-18 22:28:47 -0500

    setting up babel with webpack

---
+++ indecision_app/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      options: {
+        presets: [
+          '@babel/env', '@babel/react'
+        ]
+      },
+      exclude: /node_modules/
+    }]
+  }
+};
