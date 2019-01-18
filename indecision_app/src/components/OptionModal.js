dd274864c319e4f3167aefc40cc1632ee01a6e3a
Dan Spector <danno2423@gmail.com>
2019-01-17 22:26:46 -0500

    finished indecision app!!!

--- indecision_app/src/components/OptionModal.js
+++ indecision_app/src/components/OptionModal.js
@@ -8,10 +8,11 @@ const OptionModal = (props) => (
       isOpen={!!props.selectedOption} //converts to true boolean values
       onRequestClose={props.handleDeleteSelectedOption}
       contentLabel="Selected Option"
+      className="modal"
     >
-      <h3>Selected Option</h3>
-      {props.selectedOption && <p>{props.selectedOption}</p>}
-      <button onClick={props.handleDeleteSelectedOption}>Okay</button>
+      <h3 className="modal__title">Selected Option</h3>
+      {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+      <button className="button" onClick={props.handleDeleteSelectedOption}>Okay</button>
     </Modal>
   );
 
