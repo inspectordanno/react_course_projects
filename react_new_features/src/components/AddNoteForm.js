c866d0c62a134731b171948506881cffb826d2ed
Dan Spector <danno2423@gmail.com>
2019-06-14 00:22:21 -0400

    more react hooks

--- react_new_features/src/components/AddNoteForm.js
+++ react_new_features/src/components/AddNoteForm.js
@@ -1,10 +1,12 @@
 import React, { useState, useContext } from 'react';
 import NotesContext from '../context/notes_context';
+import useMousePosition from '../hooks/useMousePosition';
 
 const AddNoteForm = () => {
   const { dispatch } = useContext(NotesContext);
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+  const position = useMousePosition();
 
   const addNote = (e) => {
     e.preventDefault();
@@ -20,14 +22,14 @@ const AddNoteForm = () => {
   }
 
   return (
-    <div>
-      <p>Add note</p>
+    <>
+      <p>Add note {position.x} - {position.y}</p>
       <form onSubmit={addNote}>
         <input value={title} onChange={(e) => setTitle(e.target.value)} />
         <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
         <button>add note</button>
       </form>
-    </div>
+    </>
   ) 
 }
 
--- react_new_features/src/components/Note.js
+++ react_new_features/src/components/Note.js
@@ -1,8 +1,11 @@
 import React, { useContext } from 'react';
 import NotesContext from '../context/notes_context';
+import useMousePosition from '../hooks/useMousePosition';
+
 
 const Note = ({ note }) => {
   const { dispatch } = useContext(NotesContext);
+  const position = useMousePosition();
 
   const removeNote = () => {
     dispatch({
@@ -15,6 +18,7 @@ const Note = ({ note }) => {
     <div>
       <h3>{note.title}</h3>
       <p>{note.body}</p>
+      <p>{position.x}, {position.y}</p>
       <button onClick={removeNote} >x</button>
     </div>
   );
